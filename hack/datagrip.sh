7257661eadd94072c1a8ada6680d2503e6a1d7ea
Juliano Galgaro <julianogalgaro@gmail.com>
2019-04-07 11:24:56 -0300

    Add datagrip

---
+++ hack/datagrip.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DG_VERSION="2018.2"
+cd /tmp
+wget https://download.jetbrains.com/datagrip/datagrip-${DG_VERSION}.tar.gz
+tar -xzf datagrip-${DG_VERSION}.tar.gz
+
+rm -rf /opt/datagrip
+mkdir -p /opt
+mv ./DataGrip-${DG_VERSION} /opt/datagrip
+
+echo "-> If you need to restore your old config folder just move to ~/.datagrip before start the application"

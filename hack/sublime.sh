3449e0a3a46c530cb4532d921b9972e7248a7e06
Juliano Galgaro <juliano.galgaro@neoway.com.br>
2019-04-08 19:25:38 -0300

    fix flag

--- hack/sublime.sh
+++ hack/sublime.sh
@@ -6,7 +6,7 @@ echo "Installing Sublime"
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -y  -
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add  -
 
 sudo apt update
 sudo apt install -y sublime-text

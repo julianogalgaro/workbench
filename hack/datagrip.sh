8af791d12a64a8fa40c0204d914fa5a0abd19f65
Juliano Galgaro <julianogalgaro@gmail.com>
2019-04-08 19:41:04 -0300

    add sudo

--- hack/datagrip.sh
+++ hack/datagrip.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -e 
 DG_VERSION="2018.2"
 cd /tmp
 wget https://download.jetbrains.com/datagrip/datagrip-${DG_VERSION}.tar.gz
@@ -7,6 +8,6 @@ tar -xzf datagrip-${DG_VERSION}.tar.gz
 
 rm -rf /opt/datagrip
 mkdir -p /opt
-mv ./DataGrip-${DG_VERSION} /opt/datagrip
+sudo mv ./DataGrip-${DG_VERSION} /opt/datagrip
 
 echo "-> If you need to restore your old config folder just move to ~/.datagrip before start the application"

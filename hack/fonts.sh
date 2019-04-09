e74853fbc5ce850987f583bb12a547eb85821f9f
Juliano Galgaro <julianogalgaro@gmail.com>
2019-04-09 11:39:41 -0300

    update fonts

--- hack/fonts.sh
+++ hack/fonts.sh
@@ -8,3 +8,7 @@ cd /tmp
 git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
 cd awesome-terminal-fonts
 ./install.sh
+
+mkdir -p ${HOME}/.config/fontconfig/conf.d
+cp ./hack/cfg/11-powerline-symbols.conf ${HOME}/.config/fontconfig/conf.d/
+fc-cache -vf

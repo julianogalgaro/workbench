128690477a43e7fc90ade449506607001b2ff691
Juliano Galgaro <julianogalgaro@gmail.com>
2019-04-10 16:15:07 -0300

    fix fonts back to the script directory

--- hack/fonts.sh
+++ hack/fonts.sh
@@ -3,13 +3,15 @@
 set -o errexit
 set -o nounset
 
+CURRENT_DIR=$(pwd)
+
 echo "Installing some fonts"
 cd /tmp
 git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
 cd awesome-terminal-fonts
 ./install.sh
 
-cd "$(dirname "$(realpath "$0")")"
+cd ${CURRENT_DIR}
 mkdir -p ${HOME}/.config/fontconfig/conf.d
 cp ./hack/cfg/11-powerline-symbols.conf ${HOME}/.config/fontconfig/conf.d/
 fc-cache -vf

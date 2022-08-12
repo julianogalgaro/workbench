f5add2691f40769843e78f0a222b2f631fa066f0
Juliano Galgaro <julianogalgaro@gmail.com>
2022-08-12 10:21:20 -0300

    Update zsh.sh

--- hack/zsh.sh
+++ hack/zsh.sh
@@ -7,7 +7,7 @@ echo "Installing zsh"
 sudo apt install -y zsh
 
 echo "Set zsh as default shell"
-sudo chsh -s $(which zsh) $USER
+sudo chsh -s $(which zsh) "$USER"
 
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
